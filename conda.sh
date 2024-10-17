7e9112f0fea5723a5e01e8570e8c04fc343b5089
Brazilian Microbiome Project <victor.pylro@gmail.com>
2024-10-17 11:22:33 -0300

    Create conda.sh

---
+++ conda.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#BTW - Bioinformatics Through Windows - 2.0
+
+#Update and upgrade Ubuntu
+apt-get update -y
+apt-get upgrade -y
+
+#Install pip3
+apt-get install python3-pip -y
+
+#Install Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p miniconda3
+miniconda3/bin/conda init
